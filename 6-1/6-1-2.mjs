8a033573cf530527bfbcca3a79cb1d677a4a85ce
HC42-1041 <yasai.boogiewoogie1957@gmail.com>
2025-01-20 15:42:32 +0900

    6-1-2.mjs~6-2-6.mjs

---
+++ 6-1/6-1-2.mjs
@@ -0,0 +1,19 @@
+const testObject = {
+    a: 10 ,
+    b: 20 ,
+    
+    
+    //str1: "test",
+    
+    bool: true,
+    //test1: null,
+    //test2: undefined,
+    nestObject: {
+        //a += 1,
+        //a -= 1,
+        //a *= 1,
+        //a /= 1,
+    //str1: "C",
+    }
+}
+console.log(testObject)
---
+++ 6-2/6-2-1.mjs
@@ -0,0 +1,7 @@
+const array1 = [
+    "A",
+    "B",
+    "C"
+    ]
+    console.log(array1[0], array1[1], array1[2])
+    console.log(array1.length)
